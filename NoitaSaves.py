7e7bf6192ff6f882620680c69033364452a8fd34
Sedo-KFM <fedor122448@gmail.com>
2023-08-06 15:28:49 +0400

    Saving from buffer

--- NoitaSaves.py
+++ NoitaSaves.py
@@ -78,54 +78,58 @@ while scenario != 'e':
 
         if not os.path.exists(game_dir):
             print('\nError: Game files not found\n\n')
-        else:
+            continue
 
-            if scenario == 's':
+        if scenario == 's':
+            if buffer == '':
                 save_name = input('Input save name >> ')
-                save_name_errors = set(re.findall(r'[^A-Za-zА-Яа-я0-9\- ]', save_name))
-                if len(save_name_errors) > 0:
-                    print('Error: Incorrect symbols: [', end='')
-                    print(*save_name_errors, sep='], [', end=']\n')
+            else:
+                save_name = buffer
+            save_name_errors = set(re.findall(r'[^A-Za-zА-Яа-я0-9\- ]', save_name))
+            if len(save_name_errors) > 0:
+                print('Error: Incorrect symbols: [', end='')
+                print(*save_name_errors, sep='], [', end=']\n')
+            else:
+                save_name = save_name.replace(' ', '_')
+                if save_name in os.listdir(saves_dir):
+                    print('Error: This save is already exists')
+                    continue
                 else:
-                    save_name = save_name.replace(' ', '_')
-                    if save_name in os.listdir(saves_dir):
-                        print('Error: This save is already exists')
-                    else:
-                        print('Saving...')
-                        shutil.copytree(game_dir + r'\save00', saves_dir + '\\' + save_name)
-                        print('Done!')
+                    print('Saving...')
+                    shutil.copytree(game_dir + r'\save00', saves_dir + '\\' + save_name)
 
-            elif scenario in ('l', 'd'):
-                save_index = None
-                if buffer == '':
-                    buffer = input('Select the save index ' + ('(or "all")' if scenario == 'd' else '') + ' >> ')
-                if buffer.isdecimal():
-                    save_index = int(buffer)
-                    buffer = None
-                    if not 0 < save_index <= len(saves):
-                        print('Error: Incorrect index')
-                        continue
-                elif scenario == 'd' and buffer in ('a', 'al', 'all'):
-                    buffer = 'all'
-                else:
+        elif scenario in ('l', 'd'):
+            save_index = None
+            if buffer == '':
+                buffer = input('Select the save index ' + ('(or "all")' if scenario == 'd' else '') + ' >> ')
+            if buffer.isdecimal():
+                save_index = int(buffer)
+                buffer = None
+                if not 0 < save_index <= len(saves):
                     print('Error: Incorrect index')
                     continue
+            elif scenario == 'd' and buffer in ('a', 'al', 'all'):
+                buffer = 'all'
+            else:
+                print('Error: Incorrect index')
+                continue
 
-                if scenario == 'l':
-                    shutil.rmtree(game_dir + r'\save00')
-                    shutil.copytree(saves_dir + '\\' + saves[save_index - 1], game_dir + r'\save00')
-                if scenario == 'd':
-                    if buffer == 'all':
+            if scenario == 'l':
+                shutil.rmtree(game_dir + r'\save00')
+                shutil.copytree(saves_dir + '\\' + saves[save_index - 1], game_dir + r'\save00')
+
+            elif scenario == 'd':
+                if buffer == 'all':
+                    for save in saves:
+                        shutil.rmtree(saves_dir + '\\' + save)
+                else:
+                    if save_index == -1:
                         for save in saves:
                             shutil.rmtree(saves_dir + '\\' + save)
                     else:
-                        if save_index == -1:
-                            for save in saves:
-                                shutil.rmtree(saves_dir + '\\' + save)
-                        else:
-                            shutil.rmtree(saves_dir + '\\' + saves[save_index - 1])
+                        shutil.rmtree(saves_dir + '\\' + saves[save_index - 1])
 
-            print('\nDone!\n\n')
+        print('\nDone!')
 
     if scenario in ('cs-d', 'cs-w', 'rs-d', 'rs-w'):
         if not shortcuts_enabled:
